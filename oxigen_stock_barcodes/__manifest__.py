03cf1eb6572645a160d5cb72ed59005d50a94208
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-09-28 12:58:28 +0200

    [ADD] oxigen_stock_barcodes

---
+++ oxigen_stock_barcodes/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2021 ForgeFlow S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Oxigen stock barcodes",
+    "summary": "It allows to see picking information when scanning barcodes",
+    "version": "11.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://odoo-community.org/",
+    "category": "Extra Tools",
+    "depends": ["stock_barcodes"],
+    "data": ["wizard/stock_barcodes_read_picking_info_views.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
