b968eba35d8ab511fadf54df293c2fb533f24e70
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-09-29 11:45:28 +0200

    [ADD] oxigen_mrp_repair

---
+++ oxigen_mrp_repair/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Oxigen Mrp Repair",
+    "summary": "Custumizations for Oxigen in Repairs application",
+    "version": "11.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://www.forgeflow.com",
+    "category": "Manufacturing",
+    "depends": ["mrp_repair"],
+    "installable": True,
+    "license": "AGPL-3",
+}
