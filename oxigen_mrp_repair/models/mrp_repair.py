010d5eeb7f44cb1158843226040476187578421e
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-10-26 11:03:47 +0200

    [IMP] oxigen_mrp_repair: set to draft when under repair state

--- oxigen_mrp_repair/models/mrp_repair.py
+++ oxigen_mrp_repair/models/mrp_repair.py
@@ -1,7 +1,8 @@
 # Copyright 2021 ForgeFlow, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class OxigenRepair(models.Model):
@@ -9,20 +10,15 @@ class OxigenRepair(models.Model):
     _inherit = ["mrp.repair"]
 
     operations = fields.One2many(
-        states={
-            "2binvoiced": [("readonly", False)],
-            "under_repair": [("readonly", False)],
-            "draft": [("readonly", False)],
-            "confirmed": [("readonly", False)],
-            "ready": [("readonly", False)],
-        }
+        states={"draft": [("readonly", False)], "confirmed": [("readonly", False)]}
     )
     fees_lines = fields.One2many(
-        states={
-            "2binvoiced": [("readonly", False)],
-            "under_repair": [("readonly", False)],
-            "draft": [("readonly", False)],
-            "confirmed": [("readonly", False)],
-            "ready": [("readonly", False)],
-        }
+        states={"draft": [("readonly", False)], "confirmed": [("readonly", False)]}
     )
+
+    @api.multi
+    def action_repair_cancel_draft(self):
+        if self.filtered(lambda repair: repair.state not in ["cancel", "under_repair"]):
+            raise UserError(_("Repair must be canceled in order to reset it to draft."))
+        self.mapped("operations").write({"state": "draft"})
+        return self.write({"state": "draft"})
