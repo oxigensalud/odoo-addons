a5b80cd6749c7a782cce5da421ec31565668a44f
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-11-10 10:42:18 +0100

    [IMP] oxigen_mrp_repair

--- oxigen_mrp_repair/models/mrp_repair.py
+++ oxigen_mrp_repair/models/mrp_repair.py
@@ -9,16 +9,35 @@ class OxigenRepair(models.Model):
     _description = "Repair Order"
     _inherit = ["mrp.repair"]
 
+    distance_km = fields.Integer(string="Kilometers", group_operator="max")
+    list_date = fields.Datetime(string="Lists date")
     operations = fields.One2many(
-        states={"draft": [("readonly", False)], "confirmed": [("readonly", False)]}
+        states={
+            "draft": [("readonly", False)],
+            "confirmed": [("readonly", False)],
+            "under_repair": [("readonly", False)],
+        }
     )
+
     fees_lines = fields.One2many(
-        states={"draft": [("readonly", False)], "confirmed": [("readonly", False)]}
+        states={
+            "draft": [("readonly", False)],
+            "confirmed": [("readonly", False)],
+            "under_repair": [("readonly", False)],
+        }
     )
 
     @api.multi
     def action_repair_cancel_draft(self):
+        """ if MO in under_repair or cancelled states, it can be set again to draft"""
+
         if self.filtered(lambda repair: repair.state not in ["cancel", "under_repair"]):
             raise UserError(_("Repair must be canceled in order to reset it to draft."))
         self.mapped("operations").write({"state": "draft"})
         return self.write({"state": "draft"})
+
+    def unlink(self):
+        if self.state in ("done", "2binvoiced"):
+            raise UserError(_("Cannot delete a finished Repair Order."))
+
+        return super(OxigenRepair, self).unlink()
