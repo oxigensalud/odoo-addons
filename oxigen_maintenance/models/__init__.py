dd753273737e28fa68842933055bad08209f8b48
mariadforgeflow <maria.de.luna@forgeflow.com>
2021-11-24 10:40:13 +0100

    [ADD] oxigen_maintenance

---
+++ oxigen_maintenance/models/__init__.py
@@ -0,0 +1,1 @@
+from . import maintenance, stock_location
---
+++ oxigen_maintenance/models/stock_location.py
@@ -0,0 +1,27 @@
+# Copyright 2021 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import fields, models
+
+
+class StockLocation(models.Model):
+    _inherit = "stock.location"
+
+    equipment_ids = fields.One2many(
+        comodel_name="maintenance.equipment",
+        inverse_name="location_id",
+        string="Maintenance Equipment",
+    )
+
+    def action_view_equipment(self):
+        action = self.env.ref("maintenance.hr_equipment_action")
+        result = action.read()[0]
+        equipment_ids = self.equipment_ids.ids
+
+        if len(equipment_ids) != 1:
+            result["domain"] = [("id", "in", equipment_ids)]
+        else:
+            res = self.env.ref("maintenance.hr_equipment_view_form", False)
+            result["views"] = [(res and res.id or False, "form")]
+            result["res_id"] = equipment_ids[0]
+        return result
