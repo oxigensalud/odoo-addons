011be10b75b43f7a719daedc062c0532da148ecf
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-30 14:07:34 -0500

    [IMP] custom_gj: black, isort, prettier

--- custom_gj/models/res_company.py
+++ custom_gj/models/res_company.py
@@ -2,13 +2,12 @@
 # Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Company(models.Model):
     _inherit = "res.company"
 
     external_report_layout = fields.Selection(
-        selection_add=[('gj', 'GJ'),
-                       ('adda', 'AD/DA'),
-                       ])
+        selection_add=[("gj", "GJ"), ("adda", "AD/DA"),]
+    )
