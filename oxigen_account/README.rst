7bd9b473aa6d5619dfb0341ae0120bdfafc85472
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-02-17 08:56:19 +0100

    [ADD] oxigen_account

---
+++ oxigen_account/README.rst
@@ -0,0 +1,13 @@
+==================
+Oxigen Invoicing
+==================
+
+* In a vendor bill, when Bill date field is changed it sets the same value in Accounting Date field.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Maria de Luna <maria.de.luna@forgeflow.com>
---
+++ oxigen_account/models/account_move.py
@@ -0,0 +1,12 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import api, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    @api.onchange("invoice_date")
+    def _onchange_invoice_date(self):
+        self.date = self.invoice_date
