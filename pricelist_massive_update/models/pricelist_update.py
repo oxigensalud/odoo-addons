84ffb8bf6bafcc90621efc005a19c2aab6d72a6a
Lois Rilo <lois.rilo@forgeflow.com>
2022-03-15 17:59:23 +0100

    [FIX] pricelist_massive_update: default company usage

--- pricelist_massive_update/models/pricelist_update.py
+++ pricelist_massive_update/models/pricelist_update.py
@@ -10,7 +10,7 @@ class PriceListUpdate(models.Model):
     company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
-        default=lambda self: self.env.user.company_id,
+        default=lambda self: self.env.company,
     )
     price_list_tag_ids = fields.Many2many(comodel_name="pricelist.tags", string="Tags")
     percentage = fields.Float(string="Percentage:")
