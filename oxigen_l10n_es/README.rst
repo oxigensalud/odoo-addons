92e1a567cb3cdfd8b8f8b52603653f6fc1182f72
Lois Rilo <lois.rilo@forgeflow.com>
2022-03-30 09:13:57 +0200

    [ADD] oxigen_l10n_es

---
+++ oxigen_l10n_es/README.rst
@@ -0,0 +1,14 @@
+===================
+Oxigen Localization
+===================
+
+This modules contains tweaks and extensions to default PGCE (Spanish Chart
+of accounts).
+
+Credits
+=======
+
+Contributors
+------------
+
+* Lois Rilo <lois.rilo@forgeflow.com>
---
+++ oxigen_l10n_es/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Oxigen Localization",
+    "summary": "Customizations to PGCE for Oxigen",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Accounting/Localizations/Account Charts",
+    "depends": ["l10n_es"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": [
+        "data/account_chart_template_data.xml",
+    ],
+}
