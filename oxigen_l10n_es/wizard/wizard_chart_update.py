7b6027c3fb9e7f30c01473fe128aa07f92ffc01e
Lois Rilo <lois.rilo@forgeflow.com>
2022-04-12 11:41:12 +0200

    [FIX] oxigen_l10n_es: indicate the patch needed to make it work.

--- oxigen_l10n_es/wizard/wizard_chart_update.py
+++ oxigen_l10n_es/wizard/wizard_chart_update.py
@@ -9,3 +9,6 @@ class OxigenWizardUpdateChartsAccounts(models.TransientModel):
         """Override method since we DON'T want to check the condition:
         - If a parent tax is tried to be created, children taxes must be
          also included to be created."""
+        # needs to be run with this patch to be able to successfully update taxes.
+        # https://github.com/ForgeFlow/odoo/tree/14.0-do-not-delay-parent-tax-template-update
+        return True
