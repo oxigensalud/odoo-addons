94fe5f99321e2aa2172fdd0a5584658040a5948f
Miquel Raïch <miquel.raich@forgeflow.com>
2022-04-19 18:16:27 +0200

    [ADD] oxigen_l10n_es_dua

---
+++ oxigen_l10n_es_dua/README.rst
@@ -0,0 +1,13 @@
+============================
+Oxigen Importaciones con DUA
+============================
+
+This modules contains tweaks and extensions to l10n_es_dua.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Miquel Raïch <miquel.raich@forgeflow.com>
---
+++ oxigen_l10n_es_dua/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Oxigen Importaciones con DUA",
+    "summary": "Customizations to l10n_es_dua for Oxigen",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Accounting/Localizations/Account Charts",
+    "depends": [
+        "l10n_es_dua",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": [
+        "data/account_fiscal_position_template_data.xml",
+    ],
+    "auto_install": True,
+}
