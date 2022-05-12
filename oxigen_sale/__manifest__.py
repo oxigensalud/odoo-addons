2aa1860fe74996a02effbc4b5b9f1bc080f28419
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-05-12 17:13:01 +0200

    [IMP] oxigen_sale: filters for customer in sale orders

---
+++ oxigen_sale/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Oxigen Sale",
+    "summary": "Customizations for Oxigen in Sale application",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Sales",
+    "depends": ["sale"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": ["views/sale_views.xml"],
+}
