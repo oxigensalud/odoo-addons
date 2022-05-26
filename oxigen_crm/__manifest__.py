3c32562f76a62354bc6d474b6abe7657cc4fc599
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-05-26 12:09:02 +0200

    [ADD] oxigen_crm

---
+++ oxigen_crm/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Oxigen CRM",
+    "summary": "Customizations for Oxigen in CRM",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Sales/CRM",
+    "depends": ["crm"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": ["data/system_parameter.xml"],
+}
