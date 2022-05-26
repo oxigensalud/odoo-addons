be43a6573f0672e2d502938bd3c35850aa79d470
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-05-26 12:09:02 +0200

    [ADD] oxigen_crm

---
+++ oxigen_crm/README.rst
@@ -0,0 +1,16 @@
+==================
+Oxigen CRM
+==================
+
+* In opportunities with customer "Presupuesto Genérico", when email/phone are changed, don't update
+  them in the customer form.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Maria de Luna <maria.de.luna@forgeflow.com>
+
---
+++ oxigen_crm/models/crm_lead.py
@@ -0,0 +1,26 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import models
+
+
+class Lead(models.Model):
+    _inherit = "crm.lead"
+
+    def _get_partner_email_update(self):
+        self.ensure_one()
+        generic_crm_customer = int(
+            self.env["ir.config_parameter"].get_param("generic_crm_customer")
+        )
+        if self.partner_id.id == generic_crm_customer:
+            return False
+        return super()._get_partner_email_update()
+
+    def _get_partner_phone_update(self):
+        self.ensure_one()
+        generic_crm_customer = int(
+            self.env["ir.config_parameter"].get_param("generic_crm_customer")
+        )
+        if self.partner_id.id == generic_crm_customer:
+            return False
+        return super()._get_partner_phone_update()
