f1672c507bc00084759d8be80406bf157cd96e36
Miquel Raïch <miquel.raich@forgeflow.com>
2022-06-01 16:16:14 +0200

    [IMP] oxigen_account: option to set move in draft maintaining reconciliations

--- oxigen_account/models/account_move.py
+++ oxigen_account/models/account_move.py
@@ -1,7 +1,9 @@
 # Copyright 2022 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+import json
 
-from odoo import api, models
+from odoo import _, api, models
+from odoo.tools import date_utils
 
 
 class AccountMove(models.Model):
@@ -16,3 +18,32 @@ class AccountMove(models.Model):
         for move in self.filtered(lambda m: m.move_type != "entry"):
             move.payment_reference = move.ref
             move._onchange_payment_reference()
+
+    @api.depends("move_type", "line_ids.amount_residual")
+    def _compute_payments_widget_reconciled_info(self):
+        res = super()._compute_payments_widget_reconciled_info()
+        for move in self.filtered(
+            lambda l: l.state != "posted" and l.is_invoice(include_receipts=True)
+        ):
+            payments_widget_vals = {
+                "title": _("Less Payment"),
+                "outstanding": False,
+                "content": [],
+            }
+            payments_widget_vals["content"] = move._get_reconciled_info_JSON_values()
+
+            if payments_widget_vals["content"]:
+                move.invoice_payments_widget = json.dumps(
+                    payments_widget_vals, default=date_utils.json_default
+                )
+            else:
+                move.invoice_payments_widget = json.dumps(False)
+        return res
+
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    def remove_move_reconcile(self):
+        if not self.env.context.get("dont_unreconcile_items", False):
+            return super().remove_move_reconcile()
