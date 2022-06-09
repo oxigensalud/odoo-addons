eee6f7e32c2d933eaed7ff2063cff8ac30bb314a
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-06-09 16:30:09 +0200

    [ADD] oxigen_contacts

---
+++ oxigen_base/README.rst
@@ -0,0 +1,14 @@
+===========
+Oxigen Base
+===========
+
+* Default contact type as 'contact' when creating it from a Company
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Maria de Luna <maria.de.luna@forgeflow.com>
---
+++ oxigen_base/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Oxigen Base",
+    "summary": "Customizations for Oxigen in Base",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Generic Modules/Base",
+    "depends": ["base"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": ["views/res_partner_views.xml"],
+}
