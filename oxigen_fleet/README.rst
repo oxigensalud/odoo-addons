be8530ee50d23719cba5d6ecb37fce4d45657225
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-06-09 17:01:18 +0200

    [ADD] oxigen_fleet

---
+++ oxigen_fleet/README.rst
@@ -0,0 +1,14 @@
+============
+Oxigen Fleet
+============
+
+* Make possible to create vehicle tags from the vehicle form
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Maria de Luna <maria.de.luna@forgeflow.com>
---
+++ oxigen_fleet/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Oxigen Fleet",
+    "summary": "Customizations for Oxigen in Fleet",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Human Resources/Fleet",
+    "depends": ["fleet"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": ["views/fleet_vehicle_views.xml"],
+}
