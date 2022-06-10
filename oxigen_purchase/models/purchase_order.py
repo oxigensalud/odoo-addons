5501aba02c1de8deef256c0f5fd147bd918469da
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-06-10 12:15:57 +0200

    [IMP]oxigen purchase: set picking_type according to user's Default Warehouse

--- oxigen_purchase/models/purchase_order.py
+++ oxigen_purchase/models/purchase_order.py
@@ -1,7 +1,7 @@
 # Copyright 2021 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PurchaseOrder(models.Model):
@@ -13,3 +13,14 @@ class PurchaseOrder(models.Model):
     def request_validation(self):
         self.filtered(lambda r: not r.user_id).write({"user_id": self.env.uid})
         return super().request_validation()
+
+    @api.model
+    def _get_picking_type(self, company_id):
+        user_warehouse = self.env.user.property_warehouse_id
+        if user_warehouse:
+            picking_type = self.env["stock.picking.type"].search(
+                [("code", "=", "incoming"), ("warehouse_id", "=", user_warehouse.id)]
+            )
+            return picking_type[:1]
+        else:
+            return super()._get_picking_type(company_id)
