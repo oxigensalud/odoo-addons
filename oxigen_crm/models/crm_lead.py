1b254643216c16a4fc3e0ca10bdbc5c459465add
Lois Rilo <lois.rilo@forgeflow.com>
2022-06-13 12:17:43 +0200

    [IMP] oxigen_crm: allow multiple generic customer ids in parameter

--- oxigen_crm/models/crm_lead.py
+++ oxigen_crm/models/crm_lead.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 from odoo import api, fields, models
+from odoo.tools.safe_eval import safe_eval
 
 
 class Lead(models.Model):
@@ -13,11 +14,15 @@ class Lead(models.Model):
 
     @api.depends("partner_id")
     def _compute_is_generic_customer(self):
-        generic_crm_customer_id = int(
+        generic_crm_customer_param = (
             self.env["ir.config_parameter"].sudo().get_param("generic_crm_customer")
         )
+        if all(c in generic_crm_customer_param for c in ["[", ",", "]"]):
+            generic_crm_customer_ids = safe_eval(generic_crm_customer_param)
+        else:
+            generic_crm_customer_ids = [int(generic_crm_customer_param)]
         for rec in self:
-            rec.is_generic_customer = rec.partner_id.id == generic_crm_customer_id
+            rec.is_generic_customer = rec.partner_id.id in generic_crm_customer_ids
 
     def _get_partner_email_update(self):
         self.ensure_one()
