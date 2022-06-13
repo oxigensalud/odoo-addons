e95d2b81fee6586edba003935d2ad4dd119ece37
Lois Rilo <lois.rilo@forgeflow.com>
2022-06-13 12:02:31 +0200

    [IMP] oxigen_crm: do not show email and phone fields when using generic customer

--- oxigen_crm/__manifest__.py
+++ oxigen_crm/__manifest__.py
@@ -11,5 +11,5 @@
     "depends": ["crm"],
     "installable": True,
     "license": "AGPL-3",
-    "data": ["data/system_parameter.xml"],
+    "data": ["data/system_parameter.xml", "views/crm_lead_views.xml"],
 }
