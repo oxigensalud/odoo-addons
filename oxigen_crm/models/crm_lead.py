55ed3f7041b0cbd90e63455a8672360478da0105
Lois Rilo <lois.rilo@forgeflow.com>
2022-06-13 12:02:31 +0200

    [IMP] oxigen_crm: do not show email and phone fields when using generic customer

--- oxigen_crm/models/crm_lead.py
+++ oxigen_crm/models/crm_lead.py
@@ -1,26 +1,32 @@
 # Copyright 2022 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class Lead(models.Model):
     _inherit = "crm.lead"
 
-    def _get_partner_email_update(self):
-        self.ensure_one()
-        generic_crm_customer = int(
+    is_generic_customer = fields.Boolean(
+        compute="_compute_is_generic_customer",
+    )
+
+    @api.depends("partner_id")
+    def _compute_is_generic_customer(self):
+        generic_crm_customer_id = int(
             self.env["ir.config_parameter"].sudo().get_param("generic_crm_customer")
         )
-        if self.partner_id.id == generic_crm_customer:
+        for rec in self:
+            rec.is_generic_customer = rec.partner_id.id == generic_crm_customer_id
+
+    def _get_partner_email_update(self):
+        self.ensure_one()
+        if self.is_generic_customer:
             return False
         return super()._get_partner_email_update()
 
     def _get_partner_phone_update(self):
         self.ensure_one()
-        generic_crm_customer = int(
-            self.env["ir.config_parameter"].sudo().get_param("generic_crm_customer")
-        )
-        if self.partner_id.id == generic_crm_customer:
+        if self.is_generic_customer:
             return False
         return super()._get_partner_phone_update()
