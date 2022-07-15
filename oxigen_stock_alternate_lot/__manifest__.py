5f1671ad5c47e054653214145d035c4462470b44
Eric Antones <eantones@nuobit.com>
2022-07-15 17:44:24 +0200

    [ADD] oxigen_stock_alternate_lot: new module to add custom barcodes NOS and D/N

---
+++ oxigen_stock_alternate_lot/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 NuoBiT Solutions, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Oxigen Stock Alternate Lot",
+    "summary": "Alternative serial numbers",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Warehouse",
+    "depends": ["stock"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": [
+        "security/res_groups.xml",
+        "views/product_template.xml",
+        "views/stock_production_lot_views.xml",
+    ],
+}
---
+++ oxigen_stock_alternate_lot/models/product_template.py
@@ -0,0 +1,54 @@
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    nos_enabled = fields.Boolean(string="NOS")
+    dn_enabled = fields.Boolean(string="D/N")
+
+    @api.constrains("nos_enabled", "dn_enabled", "tracking")
+    def _check_alternate_lot(self):
+        for rec in self:
+            if rec.tracking == "serial":
+                if rec.dn_enabled and not rec.nos_enabled:
+                    raise ValidationError(_("NOS must be selected if D/N is selected"))
+            else:
+                if rec.nos_enabled or rec.dn_enabled:
+                    raise ValidationError(
+                        _(
+                            "NOS and D/N are not allowed for products without serial number"
+                        )
+                    )
+
+    def write(self, vals):
+        if "nos_enabled" in vals:
+            if self.nos_enabled and not vals["nos_enabled"]:
+                lots = self.env["stock.production.lot"].search(
+                    [
+                        ("product_id.product_tmpl_id", "=", self.id),
+                        ("nos", "!=", False),
+                    ]
+                )
+                if lots:
+                    raise ValidationError(
+                        _("You cannot disable NOS, it still exists lots with NOS")
+                    )
+        if "dn_enabled" in vals:
+            if self.dn_enabled and not vals["dn_enabled"]:
+                lots = self.env["stock.production.lot"].search(
+                    [
+                        ("product_id.product_tmpl_id", "=", self.id),
+                        ("dn", "!=", False),
+                    ]
+                )
+                if lots:
+                    raise ValidationError(
+                        _("You cannot disable D/N, it still exists lots with D/N")
+                    )
+        return super(ProductTemplate, self).write(vals)
