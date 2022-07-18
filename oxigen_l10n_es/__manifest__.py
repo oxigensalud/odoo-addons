59e63da1e78c377a0f7ba8d0bc5bf0ee20b8dd02
Eric Antones <eantones@nuobit.com>
2022-07-18 17:02:24 +0200

    [FIX] oxigen_l10n_es: wrong 349 report due to lack of tax mappings

--- oxigen_l10n_es/__manifest__.py
+++ oxigen_l10n_es/__manifest__.py
@@ -4,13 +4,14 @@
 {
     "name": "Oxigen Localization",
     "summary": "Customizations to PGCE for Oxigen",
-    "version": "14.0.1.0.2",
+    "version": "14.0.1.0.3",
     "author": "ForgeFlow",
     "website": "https://github.com/oxigensalud/odoo-addons",
     "category": "Accounting/Localizations/Account Charts",
     "depends": [
         "l10n_es",
         "l10n_es_aeat_mod303",
+        "l10n_es_aeat_mod349",
         "l10n_es_special_prorate",
         "account_chart_update",
         "l10n_es_dua_sii",
@@ -22,6 +23,7 @@
         "data/account_tax_data.xml",
         "data/legacy_account_tax_data.xml",
         "data/tax_code_map_mod303_data.xml",
+        "data/aeat_349_map_data.xml",
         "data/account_fiscal_position_template_data.xml",
         "data/oxigen_fiscal_position_extracomunitaria.xml",
         "data/aeat_sii_map_data.xml",
