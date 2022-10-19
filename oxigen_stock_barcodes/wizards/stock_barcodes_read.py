dca04bcc570e7ca640dee839afbee86884cbf480
mariadforgeflow <maria.de.luna@forgeflow.com>
2022-10-19 09:58:07 +0200

    [IMP] oxigen_stock_barcodes: find lot by NOS

---
+++ oxigen_stock_barcodes/wizards/stock_barcodes_read.py
@@ -0,0 +1,19 @@
+# Copyright 2022 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import models
+
+
+class WizStockBarcodesRead(models.AbstractModel):
+    _inherit = "wiz.stock.barcodes.read"
+
+    def _prepare_lot_domain(self):
+        lot = self.env["stock.production.lot"]
+        if len(self.barcode) == 6:
+            domain = [("nos", "=", self.barcode)]
+            if self.product_id:
+                domain.append(("product_id", "=", self.product_id.id))
+            lot = self.env["stock.production.lot"].search(domain)
+        if not lot:
+            domain = super()._prepare_lot_domain()
+        return domain
