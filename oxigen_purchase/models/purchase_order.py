1b8fc0c85d3dc30479de483e93ffb18a72c66488
Eric Antones <eantones@nuobit.com>
2023-07-25 15:47:31 +0200

    [IMP] oxigen_purchase: allow setting the current user as a purchase representative of a PO

--- oxigen_purchase/models/purchase_order.py
+++ oxigen_purchase/models/purchase_order.py
@@ -1,14 +1,11 @@
 # Copyright 2021 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
-
-    # Override default value
-    user_id = fields.Many2one(default=False)
 
     def request_validation(self):
         self.filtered(lambda r: not r.user_id).write({"user_id": self.env.uid})
