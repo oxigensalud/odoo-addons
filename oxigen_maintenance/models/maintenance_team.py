a0a1c0f5ed3b926ea1cfaced3e52bbabce0db9c1
Enric Tobella <etobella@creublanca.es>
2023-09-27 10:14:19 +0200

    [IMP] oxigen_maintenance: Unforce company

--- oxigen_maintenance/models/maintenance_team.py
+++ oxigen_maintenance/models/maintenance_team.py
@@ -1,11 +1,13 @@
 # Copyright 2023 Dixmit
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import models
+from odoo import fields, models
 
 
 class MaintenanceTeam(models.Model):
     _inherit = "maintenance.team"
+
+    company_id = fields.Many2one(default=lambda r: False)
 
     def _get_parent_teams(self):
         if not self:
