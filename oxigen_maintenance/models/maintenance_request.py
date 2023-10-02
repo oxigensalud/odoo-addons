027671f464168581233942a1350a4934538fe053
Enric Tobella <etobella@creublanca.es>
2023-10-02 12:51:09 +0200

    [IMP] oxigen_maintenance: Allow to define a customer inside an equipment

--- oxigen_maintenance/models/maintenance_request.py
+++ oxigen_maintenance/models/maintenance_request.py
@@ -8,3 +8,9 @@ class MaintenanceRequest(models.Model):
     _inherit = "maintenance.request"
 
     company_id = fields.Many2one(default=lambda r: False)
+    customer_id = fields.Many2one(compute="_compute_customer", store=True)
+
+    @api.depends("equipment_id")
+    def _compute_customer(self):
+        for record in self:
+            record.customer_id = record.equipment_id.customer_id
