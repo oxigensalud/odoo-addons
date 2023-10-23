1e4282e93b70695319acf44e42dba427264b0999
Enric Tobella <etobella@creublanca.es>
2023-10-23 11:01:50 +0200

    [ADD]l10n_es_aeat_mod322_special_prorate

---
+++ l10n_es_aeat_mod322_special_prorate/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2023 Dixmit
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Es Aeat Mod322 Special Prorate",
+    "summary": """
+        Add Special Prorate on AEAT model 322""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Dixmit",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "depends": [
+        "l10n_es_aeat_mod322",
+        "l10n_es_special_prorate",
+    ],
+    "data": [
+        "data/mod322.xml",
+    ],
+    "demo": [],
+}
