c9658bb4383652cdd694a94739cab68d36d2d303
FrankC013 <fcespedes@nuobit.com>
2023-11-28 12:11:30 +0100

    [FIX] oxigen_mrp: Not allow storing data in mrp type not allowed

--- oxigen_mrp/models/product_template.py
+++ oxigen_mrp/models/product_template.py
@@ -27,4 +27,21 @@ class ProductTemplate(models.Model):
                         )
                         % mrp_type
                     )
+            if "mrp_type" in vals:
+                lots = self.env["stock.production.lot"].search(
+                    [("product_id.product_tmpl_id", "=", rec.id)]
+                )
+                for lot in lots:
+                    mrp_fields = lot._get_product_mrp_workflow().get(
+                        rec.mrp_type, set()
+                    )
+                    if any([lot[field] for field in mrp_fields]):
+                        raise ValidationError(
+                            _(
+                                "You cannot change the tracking of the product,"
+                                " if you have fields with values in the lot"
+                                " related to this type (MRP) [%s]"
+                            )
+                            % rec.mrp_type
+                        )
         return super(ProductTemplate, self).write(vals)
