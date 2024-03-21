82c6743f97923dc3d966ccabf6794de3d9e77389
FrankC013 <fcespedes@nuobit.com>
2024-03-21 11:01:10 +0100

    [ADD] oxigen_stock_putaway_no_internal: new module

---
+++ oxigen_stock_putaway_no_internal/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
+# Frank Cespedes <fcespedes@nuobit.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Oxigen Putaway strategy no internal",
+    "summary": "Allow to exclude putaway strategies if they"
+    " are applied on an internal locations for Oxigen.",
+    "version": "14.0.1.0.0",
+    "category": "Warehouse Management",
+    "license": "AGPL-3",
+    "author": "NuoBiT Solutions",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "depends": ["stock_putaway_no_internal"],
+}
