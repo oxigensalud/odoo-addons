fe5c9cae33c456f83c9ecbaae3839f386f28563b
KNVx <kniubo@nuobit.com>
2024-06-17 11:28:29 +0200

    [ADD] oxigen_website_sale

---
+++ oxigen_website_sale/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Oxigen Website sale",
+    "summary": "Customizations for Oxigen for Website.",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Website",
+    "depends": ["website_sale"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": [
+        "views/product_template.xml",
+    ],
+}
---
+++ oxigen_website_sale/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Customizations for Oxigen for Website.
