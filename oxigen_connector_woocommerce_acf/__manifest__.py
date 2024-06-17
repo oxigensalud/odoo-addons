d7007e8eaf72c303e32fb9a75940d7d8254dbc3d
KNVx <kniubo@nuobit.com>
2024-06-17 11:28:17 +0200

    [ADD] oxigen_connector_wocommerce_acf

---
+++ oxigen_connector_woocommerce_acf/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Oxigen Connector WooCommerce ACF",
+    "summary": "Customizations for Oxigen in Connector WooCommerce. "
+    "Export fields created with Advanced Custom Fields (ACF) on WooCommerce.",
+    "version": "14.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "category": "Product",
+    "depends": ["oxigen_website_sale", "connector_woocommerce"],
+    "installable": True,
+    "license": "AGPL-3",
+    "data": [],
+}
