278f7e79b483c9f90ba6d01fedfa2cf07fc0f660
KNVx <kniubo@nuobit.com>
2024-08-09 18:00:32 +0200

    [ADD] oxigen_website_sale_extra_fields: new module

---
+++ oxigen_website_sale_extra_fields/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Website Sale Extra Fields",
+    "version": "14.0.0.1.0",
+    "author": "NuoBiT Solutions, S.L.",
+    "license": "AGPL-3",
+    "category": "Connector",
+    "website": "https://github.com/oxigensalud/odoo-addons",
+    "depends": [
+        "website_sale_extra_fields",
+    ],
+    "data": [
+        "views/product_template.xml",
+    ],
+    "installable": True,
+}
