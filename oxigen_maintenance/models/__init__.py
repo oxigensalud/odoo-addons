812f01a0e4de044d6959012038b8ab2c9d96e664
FrankC013 <fcespedes@nuobit.com>
2024-12-03 14:28:22 +0100

    [REM] oxigen_maintenance: remove stock location field

--- oxigen_maintenance/models/__init__.py
+++ oxigen_maintenance/models/__init__.py
@@ -1,6 +1,5 @@
 from . import maintenance_equipment
 from . import maintenance_plan
-from . import stock_location
 from . import contract_contract
 from . import maintenance_team
 from . import maintenance_equipment_operating_system
