567af6f6e4958243ee0b9dd9f941ff2b2bc9783c
??? <dgallo@nuobit.com>
2025-10-29 11:48:00 +0100

    [IMP] oxigen_sale: pre-commit auto fixes

--- oxigen_sale/__manifest__.py
+++ oxigen_sale/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Customizations for Oxigen in Sale application",
     "version": "14.0.1.0.0",
     "author": "ForgeFlow",
-    "website": "https://github.com/oxigensalud/odoo-addons",
+    "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Sales",
     "depends": ["sale", "sale_margin"],
     "installable": True,
