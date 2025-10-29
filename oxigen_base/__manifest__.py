1307744aa7e5605e793d7a98ce71c07b766f1e25
??? <dgallo@nuobit.com>
2025-10-29 18:05:52 +0100

    [IMP] oxigen_base: pre-commit auto fixes

--- oxigen_base/__manifest__.py
+++ oxigen_base/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Customizations for Oxigen in Base",
     "version": "14.0.1.0.0",
     "author": "ForgeFlow",
-    "website": "https://github.com/oxigensalud/odoo-addons",
+    "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Generic Modules/Base",
     "depends": ["base"],
     "installable": True,
