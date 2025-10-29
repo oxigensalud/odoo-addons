318cf4cd07654db58fb010516c62cdb02b778a00
??? <dgallo@nuobit.com>
2025-10-29 18:15:15 +0100

    [MIG] oxigen_base: Migration to 18.0

--- oxigen_base/__manifest__.py
+++ oxigen_base/__manifest__.py
@@ -1,15 +1,15 @@
 # Copyright 2022 ForgeFlow S.L.
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Oxigen Base",
     "summary": "Customizations for Oxigen in Base",
-    "version": "14.0.1.0.0",
-    "author": "ForgeFlow",
+    "version": "18.0.1.0.0",
+    "author": "ForgeFlow, NuoBiT Solutions SL",
     "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Generic Modules/Base",
     "depends": ["base"],
-    "installable": True,
     "license": "AGPL-3",
     "data": ["views/res_partner_views.xml"],
 }
