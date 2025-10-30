7d628cd75b42dfd18043ae76cb88c070d1a7706d
??? <dgallo@nuobit.com>
2025-10-30 12:32:59 +0100

    [IMP] oxigen_fleet: pre-commit auto fixes

--- oxigen_fleet/__manifest__.py
+++ oxigen_fleet/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Customizations for Oxigen in Fleet",
     "version": "14.0.1.0.0",
     "author": "ForgeFlow",
-    "website": "https://github.com/oxigensalud/odoo-addons",
+    "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Human Resources/Fleet",
     "depends": ["fleet"],
     "installable": True,
