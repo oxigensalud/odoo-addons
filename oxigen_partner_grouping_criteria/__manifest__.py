c5e0380465293a4178be74228e7547206fa4aee3
??? <dgallo@nuobit.com>
2025-10-30 13:16:37 +0100

    [MIG] oxigen_partner_grouping_criteria: Migration to 18.0

--- oxigen_partner_grouping_criteria/__manifest__.py
+++ oxigen_partner_grouping_criteria/__manifest__.py
@@ -1,16 +1,16 @@
 # Copyright 2021 ForgeFlow S.L.
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
     "name": "Oxigen Partner Grouping Criteria",
     "summary": "Customizations for Oxigen. This modules moves the 'Sales Invoicing "
     "Grouping Criteria' field in the partner view",
-    "version": "14.0.1.0.0",
-    "author": "ForgeFlow",
+    "version": "18.0.1.0.0",
+    "author": "ForgeFlow, NuoBiT Solutions SL",
     "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Sales Management",
     "depends": ["sale_order_invoicing_grouping_criteria"],
-    "installable": True,
     "license": "AGPL-3",
     "data": ["views/res_partner_views.xml"],
 }
