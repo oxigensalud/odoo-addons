b27d6cdbcbc56cc05374b18bb997778d9eb63c21
??? <dgallo@nuobit.com>
2025-10-30 13:05:44 +0100

    [IMP] oxigen_partner_grouping_criteria: pre-commit auto fixes

--- oxigen_partner_grouping_criteria/__manifest__.py
+++ oxigen_partner_grouping_criteria/__manifest__.py
@@ -7,7 +7,7 @@
     "Grouping Criteria' field in the partner view",
     "version": "14.0.1.0.0",
     "author": "ForgeFlow",
-    "website": "https://github.com/oxigensalud/odoo-addons",
+    "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Sales Management",
     "depends": ["sale_order_invoicing_grouping_criteria"],
     "installable": True,
