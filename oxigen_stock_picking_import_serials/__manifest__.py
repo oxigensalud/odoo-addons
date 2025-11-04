1d0937f106ac0909d737f9f6fe5ee184f64a908c
??? <dgallo@nuobit.com>
2025-11-04 11:32:13 +0100

    [MIG] oxigen_stock_picking_import_serials: Migration to 18.0

--- oxigen_stock_picking_import_serials/__manifest__.py
+++ oxigen_stock_picking_import_serials/__manifest__.py
@@ -1,20 +1,18 @@
-# Copyright NuoBiT Solutions, S.L. (<https://www.nuobit.com>)
-# Eric Antones <eantones@nuobit.com>
+# Copyright NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     "name": "Stock picking import serials for OxigenSalud",
     "summary": "Import serial numbers to a stock picking "
     "from spreadsheet file for OxigenSalud",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Warehouse Management",
     "license": "AGPL-3",
-    "author": "NuoBiT Solutions, S.L.",
+    "author": "NuoBiT Solutions SL",
     "website": "https://github.com/OCA/oxigen.odo-adodns",
     "depends": [
         "stock_picking_import_serials",
         "oxigen_stock_alternate_lot",
     ],
-    "data": [],
-    "installable": True,
 }
