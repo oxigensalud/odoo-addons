b1157a810b4b514290ca3af54111aa444948a684
??? <dgallo@nuobit.com>
2025-11-04 12:36:22 +0100

    [MIG] oxigen_stock_putaway_no_internal: Migration to 18.0

--- oxigen_stock_putaway_no_internal/models/stock_location.py
+++ oxigen_stock_putaway_no_internal/models/stock_location.py
@@ -1,17 +1,31 @@
 # Copyright NuoBiT Solutions - Frank Cespedes <fcespedes@nuobit.com>
+# Copyright 2025 NuoBiT Solutions - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import models
 
 
-class Location(models.Model):
+class StockLocation(models.Model):
     _inherit = "stock.location"
 
     def _excluded_location_usages(self):
         return ["internal"]
 
-    def _get_putaway_strategy(self, product):
-        putaway_location = super()._get_putaway_strategy(product)
+    def _get_putaway_strategy(
+        self,
+        product,
+        quantity=0,
+        package=None,
+        packaging=None,
+        additional_qty=None,
+    ):
+        putaway_location = super()._get_putaway_strategy(
+            product,
+            quantity=quantity,
+            package=package,
+            packaging=packaging,
+            additional_qty=additional_qty,
+        )
         if self.usage in self._excluded_location_usages():
-            return self.env[self._name]
+            return putaway_location.location_id
         return putaway_location
