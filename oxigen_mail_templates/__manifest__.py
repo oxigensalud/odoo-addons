6a7c6c4bd6d84f10a88ddb0abcf0c5160b5b3873
??? <dgallo@nuobit.com>
2025-11-17 15:14:44 +0100

    [IMP] oxigen_mail_templates: pre-commit auto fixes

--- oxigen_mail_templates/__manifest__.py
+++ oxigen_mail_templates/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Customizations for Oxigen in Templates",
     "version": "14.0.1.0.0",
     "author": "ForgeFlow",
-    "website": "https://github.com/oxigensalud/odoo-addons",
+    "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Accounting",
     "depends": ["account_invoice_batches"],
     "data": ["data/invoice_batches_templates.xml"],
