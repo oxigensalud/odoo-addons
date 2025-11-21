f53c96bff3055e2c10b6619a97fb7196babfcdf3
??? <dgallo@nuobit.com>
2025-11-21 09:36:26 +0100

    [MIG] oxigen_mail_templates: Migration to 18.0

--- oxigen_mail_templates/__init__.py
+++ oxigen_mail_templates/__init__.py
@@ -1,17 +1,13 @@
-from openupgradelib import openupgrade
-
-
-def _pre_init_hook(cr):
-    cr.execute(
-        """
+def _pre_init_hook(env):
+    env.cr.execute("""
         SELECT id, create_uid, write_uid, create_date, write_date
         FROM mail_template
-        WHERE name = 'Lote de factura Oxigen Salud S.A.'
-        """
-    )
-    data = cr.fetchone()
+        WHERE name->>'en_US' = %s
+    """, ('Lote de factura Oxigen Salud S.A.',))
+
+    data = env.cr.fetchone()
     if data:
-        cr.execute(
+        env.cr.execute(
             """
             INSERT INTO ir_model_data (module, name, model, res_id,
                 create_uid, write_uid, create_date, write_date)
@@ -19,7 +15,7 @@ def _pre_init_hook(cr):
                 'mail.template', %s, %s, %s, %s, %s)""",
             (data[0], data[1], data[2], data[3], data[4]),
         )
-        cr.execute(
+        env.cr.execute(
             """
             DELETE FROM ir_translation
             WHERE name LIKE 'mail.template,%%' AND res_id = %s""",
--- oxigen_mail_templates/__manifest__.py
+++ oxigen_mail_templates/__manifest__.py
@@ -9,7 +9,10 @@
     "author": "NuoBiT Solutions SL, ForgeFlow",
     "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Accounting",
-    "depends": ["account_invoice_batches"],
+    "depends": [
+        "account_invoice_batches",
+        "account_invoice_report_service"
+    ],
     "data": ["data/invoice_batches_templates.xml"],
     "pre_init_hook": "_pre_init_hook",
     "license": "AGPL-3",
