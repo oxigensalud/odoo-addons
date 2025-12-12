5d174daf8364ab28c1e8959ffa4be05faf191735
??? <dgallo@nuobit.com>
2025-12-12 09:25:27 +0100

    [IMP] oxigen_l10n_es_facturae_sale_stock_service: pre-commit auto fixes

--- oxigen_l10n_es_facturae_sale_stock_service/__manifest__.py
+++ oxigen_l10n_es_facturae_sale_stock_service/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Adds a service number to delivery note on facture XML file",
     "version": "14.0.1.0.0",
     "author": "NuoBiT",
-    "website": "https://github.com/oxigensalud/odoo-addons",
+    "website": "https://github.com/OCA/oxigen.odo-adodns",
     "category": "Accounting & Finance",
     "depends": ["l10n_es_facturae_sale_stock", "sale_order_service"],
     "license": "AGPL-3",
