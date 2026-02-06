6ccbec99b43a3ea794ab8fc1fc5aadd68aa77d92
??? <dgallo@nuobit.com>
2026-02-06 12:47:49 +0100

    [MIG] oxigen_website_sale_extra_fields: Migration to 18.0

--- oxigen_website_sale_extra_fields/__manifest__.py
+++ oxigen_website_sale_extra_fields/__manifest__.py
@@ -1,10 +1,11 @@
-# Copyright NuoBiT Solutions - Kilian Niubo <kniubo@nuobit.com>
+# Copyright NuoBiT Solutions SL - Kilian Niubo <kniubo@nuobit.com>
+# Copyright 2026 NuoBiT Solutions SL - Deniz Gallo <dgallo@nuobit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
 {
     "name": "Website Sale Extra Fields",
-    "version": "14.0.0.1.0",
-    "author": "NuoBiT Solutions, S.L.",
+    "version": "18.0.1.0.0",
+    "author": "NuoBiT Solutions SL",
     "license": "AGPL-3",
     "category": "Connector",
     "website": "https://github.com/OCA/oxigen.odo-adodns",
