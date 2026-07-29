ee6cc32ccd311ac9b6197dc4f524dc8e186ab080
Eric Antones <eantones@nuobit.com>
2026-07-29 19:15:57 +0200

    [IMP] oxigen_maintenance_gitlab: pre-commit auto fixes
    Regenerate the module generated files (manifest website key and README pages) under the repaired pre-commit configuration: the URLs repoint from the mangled template slug to the repository.

--- oxigen_maintenance_gitlab/__manifest__.py
+++ oxigen_maintenance_gitlab/__manifest__.py
@@ -9,7 +9,7 @@
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "Dixmit, NuoBiT Solutions SL",
-    "website": "https://github.com/OCA/oxigen.odo-adodns",
+    "website": "https://github.com/oxigensalud/odoo-addons",
     "depends": [
         "maintenance_project",
     ],
